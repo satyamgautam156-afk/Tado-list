8050192b5239172f54f6c1a302bb4c9bf53efebf
Satyam Gautam <satyamgautam156@gmail.com>
2026-06-02 19:35:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
-
 button.addEventListener("click", function() {
     const task = input.value;
-
-    if(task !== "") {
+if (task !== "") {
         const li = document.createElement("li");
         li.textContent = task;
 
