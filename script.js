ab7a0772f2f7c20a8c679e025850e90be87896ee
Satyam Gautam <satyamgautam156@gmail.com>
2026-06-02 19:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const taskList = document.querySelector("#taskList");
 
+button.addEventListener("click", function () {
+    const task = input.value;
+
+    if (task !== "") {
+        const li = document.createElement("li");
+        li.textContent = task;
+
+        taskList.appendChild(li);
+        input.value = "";
+    }
+});
